5c296958e814c36af298fd2ca2923b2b1d52a10e
marwin.moellers <moellers@htw-berlin.de>
2024-04-16 15:42:52 +0200

    initial

---
+++ src/main/java/org/example/backend/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package org.example.backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+    @GetMapping
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok("Hello World!");
+    }
+}
