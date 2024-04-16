6ce3e4eccb44d46fbaa4188f648f3b07610bbbc0
marwin.moellers <moellers@htw-berlin.de>
2024-04-16 15:47:43 +0200

    simple get request

--- src/main/java/org/example/backend/controller/HelloWorldController.java
+++ src/main/java/org/example/backend/controller/HelloWorldController.java
@@ -3,13 +3,14 @@ package org.example.backend.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
     @GetMapping
-    public ResponseEntity<String> helloWorld() {
-        return ResponseEntity.ok("Hello World!");
+    public ResponseEntity<String> helloWorld(@RequestParam("text") String text) {
+        return ResponseEntity.ok("Hello World!" + text);
     }
 }
