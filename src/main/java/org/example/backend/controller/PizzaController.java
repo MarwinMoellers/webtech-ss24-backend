081cb16d2abd8f307cd10ee4093d30aacd2c1532
marwin.moellers <moellers@htw-berlin.de>
2024-05-21 09:21:42 +0200

    fe

--- src/main/java/org/example/backend/controller/PizzaController.java
+++ src/main/java/org/example/backend/controller/PizzaController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RequestMapping("/pizza")
 @RestController
-@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173", "https://webtech-ss24-backend.onrender.com"})
+@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173", "https://webtech-ss24-frontend.onrender.com"})
 public class PizzaController {
 
     @GetMapping()
