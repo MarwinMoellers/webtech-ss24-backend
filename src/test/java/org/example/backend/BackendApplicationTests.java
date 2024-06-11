9363c1f91640912cdc684a24ba8a75695ba68b2f
marwin.moellers <moellers@htw-berlin.de>
2024-06-11 14:18:45 +0200

    test configuration für datenbank

---
+++ src/test/java/org/example/backend/BackendApplicationTests.java
@@ -0,0 +1,17 @@
+package org.example.backend;
+
+import org.example.backend.repository.PizzaRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
